472fbcf4dccb123acdc2df32cb9b7f43066ce105
Pawan Kumar <pawandubeyofficial@gmail.com>
2025-12-20 11:04:17 +0530

    Init Vite React TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>SQL Runner</h1>
+      <p>A lightweight SQL workspace for data analysts</p>
+    </div>
+  );
+};
+
+export default App;
+
